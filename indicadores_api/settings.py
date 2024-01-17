0350c5b4ae81da2b96b34a2eb8c51d58ed886e65
Victor Zuluaga <zrvictor@outlook.com>
2024-01-16 23:36:52 -0500

    fix statis files prod django

--- indicadores_api/settings.py
+++ indicadores_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 from dj_database_url import parse as db_url
 from decouple import config
 
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR + "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
